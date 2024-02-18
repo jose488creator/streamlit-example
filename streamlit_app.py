ff2869056a0930643199700988b616ca3f93162f
jose488creator <160374651+jose488creator@users.noreply.github.com>
2024-02-17 22:47:56 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,36 +1,101 @@
-import streamlit as st
+import glob
+import os
+import re
+
 import pandas as pd
-import numpy as np
+import streamlit as st
+from streamlit.locale import gettext as _
+from PIL import Image
 
-st.title('la jovana no le gusta bañarse ')
+st.set_page_config(layout="wide")
 
-DATE_COLUMN = 'date/time'
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+query_params = st.experimental_get_query_params()
 
-@st.cache_data
-def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
-    lowercase = lambda x: str(x).lower()
-    data.rename(lowercase, axis='columns', inplace=True)
-    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-    return data
+selected_language = st.session_state["language"]
 
-data_load_state = st.text('Loading data...')
-data = load_data(10000)
-data_load_state.text("Done! (using st.cache_data)")
 
-if st.checkbox('Show raw data'):
-    st.subheader('Raw data')
-    st.write(data)
+def update_params():
+    st.experimental_set_query_params(
+        challenge=st.session_state.day)
 
-st.subheader('Number of pickups by hour')
-hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
-st.bar_chart(hist_values)
 
-# Some number in the range 0-23
-hour_to_filter = st.slider('hour', 0, 23, 17)
-filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+def format_day(label):
+    return _("Day {day}").format(day=int(re.search(r'\d+', label).group()))
 
-st.subheader('Map of all pickups at %s:00' % hour_to_filter)
-st.map(filtered_data)
+
+md_files = sorted(
+    [int(x.strip("Day").strip(".md")) for x in glob.glob1(f"content/{selected_language}", "*.md")]
+)
+
+placeholder = st.empty()
+with placeholder:
+    st.write(_("Day {day}").format(day=1))
+placeholder.empty()
+
+# Logo and Navigation
+col1, col2, col3 = st.columns((1, 4, 1))
+with col2:
+    st.image(Image.open("streamlit-logo-secondary-colormark-darktext.png"))
+st.markdown(_("# 30 Days of Streamlit"))
+
+days_list = [f"Day{x}" for x in md_files]
+
+if query_params:
+    try:
+        selected_day = query_params["challenge"][0]
+        if selected_day in days_list:
+            st.session_state.day = selected_day
+    except KeyError:
+        st.session_state.day = days_list[0]
+
+selected_day = st.selectbox(
+    _("Start the Challenge 👇"), days_list, key="day", on_change=update_params,
+    format_func=format_day
+)
+
+with st.expander(_("About the #30DaysOfStreamlit")):
+    st.markdown(_(
+        """
+    The **#30DaysOfStreamlit** is a coding challenge designed to help you get started in building Streamlit apps.
+    
+    Particularly, you'll be able to:
+    - Set up a coding environment for building Streamlit apps
+    - Build your first Streamlit app
+    - Learn about all the awesome input/output widgets to use for your Streamlit app
+    """
+    ))
+
+# Sidebar
+st.sidebar.header(_("About"))
+st.sidebar.markdown(_(
+    "[Streamlit](https://streamlit.io) is a Python library that allows the creation of interactive, data-driven web applications in Python."
+))
+
+st.sidebar.header(_("Resources"))
+st.sidebar.markdown(_(
+    """
+- [Streamlit Documentation](https://docs.streamlit.io/)
+- [Cheat sheet](https://docs.streamlit.io/library/cheatsheet)
+- [Book](https://www.amazon.com/dp/180056550X) (Getting Started with Streamlit for Data Science)
+- [Blog](https://blog.streamlit.io/how-to-master-streamlit-for-data-science/) (How to master Streamlit for data science)
+"""
+))
+
+st.sidebar.header(_("Deploy"))
+st.sidebar.markdown(_(
+    "You can quickly deploy Streamlit apps using [Streamlit Community Cloud](https://streamlit.io/cloud) in just a few clicks."
+))
+
+# Display content
+for day in days_list:
+    if selected_day == day:
+        st.markdown(_("# 🗓️ Which {day_num}").format(day_num=int(re.search(r'\d+', day).group())))
+        with open(f"content/{selected_language}/{day}.md", "r") as f:
+            st.markdown(f.read())
+        if os.path.isfile(f"content/{selected_language}/figures/{day}.csv"):
+            st.markdown("---")
+            st.markdown(_("### Figures"))
+            df = pd.read_csv(f"content/{selected_language}/figures/{day}.csv", engine="python")
+            for i in range(len(df)):
+                st.image(f"content/{selected_language}/images/{df.img[i]}")
+                st.info(f"{df.figure[i]}: {df.caption[i]}")
